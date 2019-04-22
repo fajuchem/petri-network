08af1603049a0faf840d2fb87c7ac5339f04e14a
Felipe Juchem <fajuchem@gmail.com>
2019-04-22 13:46:56 -0300

    finalizado

--- src/PetriNetwork.php
+++ src/PetriNetwork.php
@@ -5,6 +5,8 @@ namespace App;
 use App\Lugar;
 use App\Arco;
 use App\Transicoes;
+use LucidFrame\Console\ConsoleTable;
+use Wujunze\Colors;
 
 class PetriNetwork
 {
@@ -16,8 +18,15 @@ class PetriNetwork
 
     public $tabela;
 
+    public $tabelaTemHeader = false;
+
+    public $tabelaLinha = 0;
+
     public function __construct($dados)
     {
+        $this->table = new ConsoleTable();
+        $this->colors = new Colors();
+
         foreach ($dados['lugares'] as $lugar) {
             $arcos = [];
             foreach ($lugar['arcos'] as $arco) {
@@ -48,15 +57,23 @@ class PetriNetwork
             foreach ($this->lugaresAponta as $transicao => $lugares) {
                 if ($this->transicoes[$transicao]->habilitado) {
                     $pes = 0;
-                    $marcas = 0;
                     foreach ($lugares as $lugar) {
                         foreach ($this->lugares[$lugar]->arcos as $ar) {
                             $pes += $ar->peso;
                         }
-                        $marcas += $this->lugares[$lugar]->marcas;
+                    }
+
+                    $marcas = 0;
+                    foreach ($lugares as $lugar) {
                         $arco = reset($this->lugares[$lugar]->arcos);
                         if ($pes > 0) {
-                            $this->lugares[$lugar]->marcas -= $arco->peso ? $arco->peso : 1;
+                            $marcas = 1;
+                        } else {
+                            $marcas = $this->lugares[$lugar]->marcas;
+                        }
+
+                        if ($pes > 0) {
+                            $this->lugares[$lugar]->marcas -= $arco->peso > 0 ? $arco->peso : 1;
                         } elseif ($arco->peso == 0) {
                             $this->lugares[$lugar]->marcas = 0;
                         } else {
@@ -64,7 +81,7 @@ class PetriNetwork
                         }
                     }
                     foreach ($this->transicoes[$transicao]->arcos as $arco) {
-                        $this->lugares[$arco->apontando]->marcas = $pes > 0 ? 1 : $marcas;
+                        $this->lugares[$arco->apontando]->marcas += $arco->peso > 0 ? $arco->peso : $marcas;
                     }
                 }
             }
@@ -101,35 +118,31 @@ class PetriNetwork
 
     private function imprimirTabela()
     {
-        $header = 'n | ';
-
-        foreach ($this->lugares as $lugar) {
-            $header .= $lugar->nome . ' ';
-        }
-
-        foreach ($this->transicoes as $transicao) {
-            $header .= $transicao->nome . ' ';
-        }
-
-        echo $header . PHP_EOL;
-
-        foreach ($this->tabela as $key => $linha) {
-            echo "$key | $linha";
-        }
+        $this->table->display();
     }
 
     private function adicionarParaTabela()
     {
-        $linha = '';
+        if (!$this->tabelaTemHeader) {
+            $this->table->addHeader('Núm');
+            foreach ($this->lugares as $lugar) {
+                $this->table->addHeader($lugar->nome);
+            }
 
+            foreach ($this->transicoes as $transicao) {
+                $this->table->addHeader($transicao->nome);
+            }
+            $this->tabelaTemHeader = true;
+        }
+
+        $row = $this->table->addRow();
+        $row->addColumn($this->tabelaLinha++);
         foreach ($this->lugares as $lugar) {
-            $linha .= $lugar->marcas . '  ';
+            $row->addColumn($lugar->marcas > 0 ? $this->colors->getColoredString($lugar->marcas, 'yellow') : $lugar->marcas);
         }
 
         foreach ($this->transicoes as $transicao) {
-            $linha .= $transicao->habilitado ? 'S  ' : 'N  ';
+            $row->addColumn($transicao->habilitado ? $this->colors->getColoredString('S', 'green') : $this->colors->getColoredString('N', 'red'));
         }
-
-        $this->tabela[] = $linha . PHP_EOL;
     }
 }
